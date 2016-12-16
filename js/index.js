7e79f903123f2ada404c46477faa6fe7ef34d091
mr_K2 <kuval2.2@gmail.com>
2016-12-16 12:42:35 +0000

    initiall

---
+++ js/index.js
@@ -0,0 +1,150 @@
+$(function() {
+    $('#bilplu').click(function() {
+        var bil = Number($('#bil').html());
+        bil++;
+        $('#bil').html(bil);
+    });
+});
+$(function() {
+    $('#bildes').click(function() {
+        var bil = Number($('#bil').html());
+        bil = bil + 10;
+        $('#bil').html(bil);
+    });
+});
+$(function() {
+    $('#bilmin').click(function() {
+        var bil = Number($('#bil').html());
+        bil--;
+        $('#bil').html(bil);
+    });
+});
+
+
+
+
+$(function() {
+    $('#cherplu').click(function() {
+        var cher = Number($('#cher').html());
+        cher++;
+        $('#cher').html(cher);
+    });
+});
+$(function() {
+    $('#cherdes').click(function() {
+        var cher = Number($('#cher').html());
+        cher = cher + 10;
+        $('#cher').html(cher);
+    });
+});
+$(function() {
+    $('#chermin').click(function() {
+        var cher = Number($('#cher').html());
+        cher--;
+        $('#cher').html(cher);
+    });
+});
+
+
+
+
+$(function() {
+    $('#perplu').click(function() {
+        var per = Number($('#per').html());
+        per++;
+        $('#per').html(per);
+    });
+});
+$(function() {
+    $('#perdes').click(function() {
+        var per = Number($('#per').html());
+        per = per + 10;
+        $('#per').html(per);
+    });
+});
+$(function() {
+    $('#permin').click(function() {
+        var per = Number($('#per').html());
+        per--;
+        $('#per').html(per);
+    });
+});
+
+
+
+
+$(function() {
+    $('#stplu').click(function() {
+        var st = Number($('#st').html());
+        st++;
+        $('#st').html(st);
+    });
+});
+$(function() {
+    $('#stdes').click(function() {
+        var st = Number($('#st').html());
+        st = st + 10;
+        $('#st').html(st);
+    });
+});
+$(function() {
+    $('#stmin').click(function() {
+        var st = Number($('#st').html());
+        st--;
+        $('#st').html(st);
+    });
+});
+
+
+
+
+$(function() {
+    $('#rexplu').click(function() {
+        var rex = Number($('#rex').html());
+        rex++;
+        $('#rex').html(rex);
+    });
+});
+$(function() {
+    $('#rexdes').click(function() {
+        var rex = Number($('#rex').html());
+        rex = rex + 10;
+        $('#rex').html(rex);
+    });
+});
+$(function() {
+    $('#rexmin').click(function() {
+        var rex = Number($('#rex').html());
+        rex--;
+        $('#rex').html(rex);
+    });
+});
+
+
+
+
+$(function() {
+    $('#slaplu').click(function() {
+        var sla = Number($('#sla').html());
+        sla++;
+        $('#sla').html(sla);
+    });
+});
+$(function() {
+    $('#slades').click(function() {
+        var sla = Number($('#sla').html());
+        sla = sla + 10;
+        $('#sla').html(sla);
+    });
+});
+$(function() {
+    $('#slamin').click(function() {
+        var sla = Number($('#sla').html());
+        sla--;
+        $('#sla').html(sla);
+    });
+});
+
+
+
+
